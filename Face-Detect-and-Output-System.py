a44f7d6c3a7b2f52e7eef08a2bfe2193a7ed1ff8
wangyirun-ch <2125664650@qq.com>
2023-09-30 03:14:10 +0800

    更新 Face-Detect-and-Output-System.py

--- Face-Detect-and-Output-System.py
+++ Face-Detect-and-Output-System.py
@@ -13,9 +13,9 @@ def get_folder_size(folder):
             size+=os.path.getsize(os.path.join(root,file))
     return size
 def enterformat(put):
-    print(put,"picture format? Input'jpg','jpeg','png','bmp',etc.")
+    print(put,"picture format? Input'jpg','png','bmp',etc.")
     putpicformat=input('')
-    while putpicformat!='jpg'and putpicformat!='JPG'and putpicformat!='jpeg' and putpicformat!='JPEG' and putpicformat!='png'and putpicformat!='PNG'and putpicformat!='bmp' and putpicformat!='BMP':
+    while putpicformat!='jpg'and putpicformat!='JPG' and putpicformat!='png'and putpicformat!='PNG'and putpicformat!='bmp' and putpicformat!='BMP':
         print("You have entered an illegal format name, please re-enter it.")
         putpicformat=input('')
     return putpicformat
@@ -53,40 +53,24 @@ while mode != 1 and mode!= 2:
     print("You have entered an illegal mode, please re-enter it.")
     mode=int(input(''))
 print("Output folder path:",outpath)
-print("")
-outputpicformat=enterformat('Output')
-if mode==2:
-    framerate=entervalue(60,1,"Frame rate")
-speed=entervalue(10,0,"Detece speed")
-accuracy=entervalue(10,1,"Detece accuracy")
-scale=round(1.05+(speed)/100,2)
-neighbors=int(accuracy//3)+3
-minsz=int(accuracy*2)+10
-print("")
-print("Detect Factor: scaleFactor=",scale,", minNeighbors=",neighbors,", minSize= (",minsz,",",minsz,")",sep='')        
-print("If you have already confirmed the settings, please enter 'start' to start processing.")
-confirm=input('')
-while confirm!='start':
-    print("If you have already confirmed the content in the 'Input' folder, please enter 'start' to start processing.")
-    confirm=input('')
 
-if mode==2:
-    cam=cv2.VideoCapture(0)
-    while(cam.isOpened()):
-        flag,frame=cam.read()
-        framenum=framenum+1
-        detect(frame,scale,neighbors,minsz,'Camera',framenum)
-        cv2.imshow('Face-Detect-and-Output-System',frame)
-        key=cv2.waitKey(int(1000/framerate))
-        if key==27:
-            break
-    cam.release()
-    cv2.destroyWindow('Face-Detect-and-Output-System')
 if mode==1:
     inpath=current + '\Input'
     print("Input folder path:",inpath)
+    print('')
     print("Please put pending pictures into 'Input' folder!")
     os.startfile(inpath)
+    print('')
+    outputpicformat=enterformat('Output')
+    speed=entervalue(10,0,"Detece speed")
+    accuracy=entervalue(10,1,"Detece accuracy")
+    scale=round(1.05+(speed)/100,2)
+    neighbors=int(accuracy//3)+3
+    minsz=int(accuracy*2)+10
+    print("")
+    print("Detect Factor: scaleFactor=",scale,", minNeighbors=",neighbors,", minSize= (",minsz,",",minsz,")",sep='')   
+    print("If you have already confirmed the settings, please press 'Enter' key to start processing.")
+    confirm=input()
     file_list=os.listdir(inpath)
     out_file_num=len(os.listdir(outpath))
     maxnum=len(file_list)
@@ -100,11 +84,13 @@ if mode==1:
         print("")
         print("Done!")
         os.system("pause")
+        exit()
     elif out_file_num!=0:
         print("")
         print("There are already some files located in the 'Output' folder!")
         print("To prevent data loss caused by overwriting, please empty all files in the 'Output' folder!")
         os.system("pause")
+        exit()
     else:
         for name in file_list:
             extension = os.path.splitext(name)[-1]
@@ -130,11 +116,11 @@ if mode==1:
             print("Total size of legal input pictures:",inputsize_byte,"Byte.")
         print("Picture format:")
         if jpgnum!=0:
-            print("JPEG format picture number:",jpgnum)
+            print("    JPG format picture number:",jpgnum)
         if pngnum!=0:
-            print("PNG format picture number:",pngnum)
+            print("    PNG format picture number:",pngnum)
         if bmpnum!=0:
-            print("BMP format picture number:",bmpnum)
+            print("    BMP format picture number:",bmpnum)
         print("")
         print("Start Processing!")
         print("")
@@ -146,6 +132,36 @@ if mode==1:
                 facenumber=detect(imgread,scale,neighbors,minsz,read,0)
                 print("    Picture",read,":",facenumber,"Faces")
         print("")
+if mode==2:
+    outputpicformat=enterformat('Output')
+    framerate=entervalue(60,1,"Frame rate")
+    speed=entervalue(10,0,"Detece speed")
+    accuracy=entervalue(10,1,"Detece accuracy")
+    scale=round(1.05+(speed)/100,2)
+    neighbors=int(accuracy//3)+3
+    minsz=int(accuracy*2)+10
+    print("")
+    print("Detect Factor: scaleFactor=",scale,", minNeighbors=",neighbors,", minSize= (",minsz,",",minsz,")",sep='')
+    out_file_num=len(os.listdir(outpath))
+    if out_file_num!=0:
+        print("")
+        print("There are already some files located in the 'Output' folder!")
+        print("To prevent data loss caused by overwriting, please empty all files in the 'Output' folder!")
+        os.system("pause")
+        exit()
+    cam=cv2.VideoCapture(0)
+    print("If you have already confirmed the settings, please press 'Enter' key to start processing.")
+    confirm=input()
+    while(cam.isOpened()):
+        flag,frame=cam.read()
+        framenum=framenum+1
+        detect(frame,scale,neighbors,minsz,'Camera',framenum)
+        cv2.imshow('Face-Detect-and-Output-System',frame)
+        key=cv2.waitKey(int(1000/framerate))
+        if key==27:
+            break
+    cam.release()
+    cv2.destroyWindow('Face-Detect-and-Output-System')
 print("Done!")
 print("")
 total=len(os.listdir(outpath))
